b5370027d86e824d42659335ac9b166b505456b4
Quick Lotter <quicklotter@hotmail.com>
2025-12-06 21:05:39 -0500

    updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,14 @@ module.exports = {
     {
       name: "scraperusa_v2",
       script: "dist/scheduler/scheduler.js",
-      cwd: "/opt/scraperusa_v2/current",
+      cwd: "./",
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: "500M",
-      env: { NODE_ENV: "production" },
+      env: {
+        NODE_ENV: "production",
+      },
     },
   ],
 
@@ -16,9 +18,16 @@ module.exports = {
     production: {
       user: "client_269_1",
       host: "64.71.161.251",
+
+      key: "/home/client_269_1/.ssh/id_ed25519",
+
       ref: "origin/main",
       repo: "git@github.com:QuickLotter/scraperusa_v2.git",
+
       path: "/opt/scraperusa_v2",
+
+      "pre-deploy-local": "",
+
       "post-deploy":
         "npm install && npm run build && pm2 reload ecosystem.config.js --only scraperusa_v2",
     },
