2fa3d9af20ea6a4cbcadf9bf4a783aacca8eb036
Quick Lotter <quicklotter@hotmail.com>
2025-12-06 19:23:48 -0500

    remove duplicated copy files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,11 +18,22 @@ module.exports = {
     production: {
       user: "client_269_1",
       host: "64.71.161.251",
+
       ref: "origin/main",
       repo: "git@github.com:QuickLotter/scraperusa_v2.git",
+
+      // 🟩 Onde o PM2 colocará o código ao fazer deploy
       path: "/opt/scraperusa_v2",
-      "post-deploy":
-        "npm install && npm run build && pm2 reload ecosystem.config.js --only scraperusa_v2",
+
+      // 🟦 Antes de enviar arquivos (local)
+      "pre-deploy-local": "",
+
+      // 🟪 Comandos executados NO VPS após receber o código
+      "post-deploy": `
+        npm install &&
+        npm run build &&
+        pm2 reload ecosystem.config.js --only scraperusa_v2
+      `,
     },
   },
 };
